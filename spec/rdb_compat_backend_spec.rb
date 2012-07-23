1dbd838378c2196437028b74977204367fdd426f
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-07-23 16:41:13 -0700

    added test codes for resource limit

--- spec/rdb_compat_backend_spec.rb
+++ spec/rdb_compat_backend_spec.rb
@@ -36,5 +36,35 @@ describe Backend::RDBCompatBackend do
     t.type.should == 'query'
     t.key.should == 'query.379474'
   end
+
+  it 'resource limit' do
+    time = Time.now.to_i
+
+    3.times do |i|
+      queue.submit("test_#{i}", 'user01', {}, :now=>time-(i+1), :user=>'u1', :max_running=>2)
+    end
+    queue.submit("test_5", 'user02', {}, :now=>time, :user=>'u2', :max_running=>2)
+
+    task1 = queue.poll(:now=>time+10)
+    task1.should_not == nil
+    task1.type.should == 'user01'
+
+    task2 = queue.poll(:now=>time+10)
+    task2.should_not == nil
+    task2.type.should == 'user02'
+
+    task3 = queue.poll(:now=>time+10)
+    task3.should_not == nil
+    task3.type.should == 'user01'
+
+    task4 = queue.poll(:now=>time+10)
+    task4.should == nil
+
+    task1.finish!
+
+    task5 = queue.poll(:now=>time+10)
+    task5.should_not == nil
+    task5.type.should == 'user01'
+  end
 end
 
