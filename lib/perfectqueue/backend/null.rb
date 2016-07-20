cb9bf93a3004b65295d26829d8d79ed016ec4f73
NARUSE, Yui <naruse@airemix.jp>
2016-07-20 17:17:31 +0900

    Merge v07 branch

---
+++ lib/perfectqueue/backend/null.rb
@@ -0,0 +1,33 @@
+
+module PerfectQueue
+
+
+class NullBackend
+  def list(&block)
+    nil
+  end
+
+  def acquire(timeout, now=Time.now.to_i)
+    nil
+  end
+
+  def finish(token, delete_timeout=3600, now=Time.now.to_i)
+    true
+  end
+
+  def update(token, timeout)
+    nil
+  end
+
+  def cancel(id, delete_timeout=3600, now=Time.now.to_i)
+    true
+  end
+
+  def submit(id, data, time=Time.now.to_i, resource=nil, max_running=nil)
+    true
+  end
+end
+
+
+end
+
