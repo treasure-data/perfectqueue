4c321c07a5d15dc0d1be8b8d93e8961299719a99
NARUSE, Yui <nurse@users.noreply.github.com>
2016-07-26 19:09:50 +0900

    Merge v07 branch (#45)
    Merge v07 branch

---
+++ lib/perfectqueue/backend/rdb.rb
@@ -0,0 +1,82 @@
+require 'sequel'
+require 'uri'
+require_relative 'rdb_compat'
+
+module PerfectQueue::Backend
+  class RDBBackend
+    MAX_RETRY = ::PerfectQueue::Backend::RDBCompatBackend::MAX_RETRY
+    DELETE_OFFSET = ::PerfectQueue::Backend::RDBCompatBackend::DELETE_OFFSET
+    class Token < Struct.new(:key)
+    end
+
+    def initialize(uri, table, config={})
+      @uri = uri
+      @table = table
+
+      u = URI.parse(@uri)
+      options = {
+        max_connections: 1,
+        user: u.user,
+        password: u.password,
+        host: u.host,
+        port: u.port ? u.port.to_i : 3306
+      }
+      options[:sslca] = config[:sslca] if config[:sslca]
+      db_name = u.path.split('/')[1]
+      @db = Sequel.mysql2(db_name, options)
+
+      @mutex = Mutex.new
+      connect {
+        # connection test
+      }
+    end
+
+    attr_reader :db
+
+    def submit(id, data, time=Process.clock_gettime(Process::CLOCK_REALTIME, :second), resource=nil, max_running=nil)
+      connect {
+        begin
+          data = Sequel::SQL::Blob.new(data)
+          @db.sql_log_level = :debug
+          n = @db["INSERT INTO `#{@table}` (id, timeout, data, created_at, resource, max_running) VALUES (?, ?, ?, ?, ?, ?);", id, time, data, time, resource, max_running].insert
+          return true
+        rescue Sequel::UniqueConstraintViolation => e
+          return nil
+        end
+      }
+    end
+
+    def cancel(id, delete_timeout=3600, now=Process.clock_gettime(Process::CLOCK_REALTIME, :second))
+      connect {
+        n = @db["UPDATE `#{@table}` SET timeout=?, created_at=NULL, resource=NULL WHERE id=? AND created_at IS NOT NULL;", now+delete_timeout-DELETE_OFFSET, id].update
+        return n > 0
+      }
+    end
+
+    private
+    def connect(&block)
+      @mutex.synchronize do
+        retry_count = 0
+        begin
+          block.call
+        rescue
+          # workaround for "Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction" error
+          if $!.to_s.include?('try restarting transaction')
+            err = ([$!] + $!.backtrace.map {|bt| "  #{bt}" }).join("\n")
+            retry_count += 1
+            if retry_count < MAX_RETRY
+              STDERR.puts err + "\n  retrying."
+              sleep 0.5
+              retry
+            else
+              STDERR.puts err + "\n  abort."
+            end
+          end
+          raise
+        ensure
+          @db.disconnect
+        end
+      end
+    end
+  end
+end
---
+++ spec/rdb_backend_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+require 'perfectqueue/backend/rdb'
+
+describe Backend::RDBBackend do
+  let (:now){ Time.now.to_i }
+  let (:uri){ 'mysql2://root:@localhost/perfectqueue_test' }
+  let (:table){ 'test_queues' }
+  let (:db) do
+    d = Backend::RDBCompatBackend.new(double, url: uri, table: table)
+    s = d.db
+    s.tables.each{|t| s.drop_table(t) }
+    d.init_database({})
+    Backend::RDBBackend.new(uri, table)
+  end
+
+  context '.new' do
+    it 'supports mysql' do
+      expect(Backend::RDBBackend.new(uri, table)).to be_an_instance_of(Backend::RDBBackend)
+    end
+  end
+
+  context '#submit' do
+    it 'adds task' do
+      db.submit('key', '{"foo":"bar"}')
+      row = db.db.fetch("SELECT * FROM `#{table}` WHERE id=? LIMIT 1", 'key').first
+      expect(row[:created_at]).not_to be_nil
+      expect(row[:data]).to eq('{"foo":"bar"}')
+    end
+  end
+
+  context '#cancel' do
+    let (:key){ 'key' }
+    let (:task_token){ Backend::RDBBackend::Token.new(key) }
+    let (:retention_time) { 42 }
+    let (:delete_timeout){ now - Backend::RDBBackend::DELETE_OFFSET + retention_time }
+    let (:options){ {now: now} }
+    context 'have the task' do
+      before do
+        db.submit(key, '{}')
+      end
+      it 'returns true' do
+        expect(db.cancel(key)).to be true
+        row = db.db.fetch("SELECT created_at FROM `#{table}` WHERE id=? LIMIT 1", key).first
+        expect(row[:created_at]).to be_nil
+      end
+    end
+    context 'already canceled' do
+      it 'returns false' do
+        expect(db.cancel(key)).to be false
+      end
+    end
+  end
+
+  context '#connect' do
+    context 'normal' do
+      it 'returns nil' do
+        expect(db.__send__(:connect){ }).to be_nil
+      end
+    end
+    context 'error' do
+      it 'returns block result' do
+        expect(RuntimeError).to receive(:new).exactly(Backend::RDBBackend::MAX_RETRY).and_call_original
+        allow(STDERR).to receive(:puts)
+        allow(db).to receive(:sleep)
+        expect do
+          db.__send__(:connect) do
+            raise RuntimeError.new('try restarting transaction')
+          end
+        end.to raise_error(RuntimeError)
+      end
+    end
+  end
+end
